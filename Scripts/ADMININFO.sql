0a9542466b8d345a82ab7a998bc69e67afb0aee3
jackson0720 <jackson0720@outlook.com>
2020-04-06 22:03:18 +0800

    新增数据库脚本文件

---
+++ Scripts/ADMININFO.sql
@@ -0,0 +1,3 @@
+INSERT INTO [ADMININFO] VALUES ('admin', '总经理'); GO
+INSERT INTO [ADMININFO] VALUES ('cashmin', '财务经理'); GO
+INSERT INTO [ADMININFO] VALUES ('hotelmin', '酒店经理'); GO
---
+++ Scripts/BACKINFO.sql
@@ -0,0 +1,2 @@
+INSERT INTO [BACKINFO] VALUES ('B001', '一次性毛巾', '超市', 1000.00, 1, '李杰峰', '财务部', '餐饮部'); GO
+INSERT INTO [BACKINFO] VALUES ('B002', '一次性牙刷', '超市', 2000.00, 2, '李杰峰', '财务部', '酒店部'); GO
