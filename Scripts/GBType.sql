ef6e5b7866eac8ff8c2ef1b2bcfde43709041884
jackson0720 <jackson0720@outlook.com>
2020-04-21 22:49:25 +0800

    更新数据库脚本2020-04-21 22:40

---
+++ Scripts/GBType.sql
@@ -0,0 +1,2 @@
+INSERT INTO [GBType]([GBType], [GBName]) VALUES (0, '奖励'); GO
+INSERT INTO [GBType]([GBType], [GBName]) VALUES (1, '惩罚'); GO
---
+++ Scripts/WorkerGoodBad.sql
@@ -0,0 +1,1 @@
+INSERT INTO [WorkerGoodBad]([WorkNo], [GBInfo], [GBType], [GBOperation], [GBTime]) VALUES ('WK008', '牛逼就完事了！', 0, '杨俊杰', '2020-04-21 21:02:30.000'); GO
