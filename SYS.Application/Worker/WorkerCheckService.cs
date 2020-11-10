009e4ffedc41a187b8346766e447c9498a72c99d
yjj0720 <jackson0720@outlook.com>
2020-11-10 14:23:45 +0800

    InitProject_fixed

---
+++ SYS.Application/Worker/WorkerCheckService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Core;
+
+namespace SYS.Application
+{
+    public class WorkerCheckService
+    {
+        /// <summary>
+        /// 根据员工编号查询其所有的打卡记录
+        /// </summary>
+        /// <param name="wid"></param>
+        /// <returns></returns>
+        public static List<WorkerCheck> SelectCheckInfoByWorkerNo(string wid)
+        {
+            List<WorkerCheck> workerChecks = new List<WorkerCheck>();
+            string sql = "select * from WorkerCheck where WorkerNo = '" + wid + "'";
+            DBHelper.Opencon();
+            SqlDataReader dr = DBHelper.ExecuteReader(sql);
+            while (dr.Read())
+            {
+                WorkerCheck workerCheck = new WorkerCheck();
+                workerCheck.WorkerNo = dr["WorkerNo"].ToString();
+                workerCheck.CheckTime = (DateTime)dr["CheckTime"];
+                workerCheck.CheckWay = dr["CheckWay"].ToString();
+                if (Convert.ToInt32(dr["CheckState"]) == 0)
+                {
+                    workerCheck.CheckState = "打卡成功";
+                }
+                else
+                {
+                    workerCheck.CheckState = "打卡失败";
+                }
+                workerChecks.Add(workerCheck);
+            }
+            dr.Close();
+            DBHelper.Closecon();
+            return workerChecks;
+        }
+
+
+        /// <summary>
+        /// 查询员工签到天数
+        /// </summary>
+        /// <param name="wkn"></param>
+        /// <returns></returns>
+        public static object SelectWorkerCheckDaySumByWorkerNo(string wkn)
+        {
+            string sql = "SELECT COUNT(*) FROM WORKERCHECK WHERE workerNo = '" + wkn + "'";
+            return DBHelper.ExecuteScalar(sql);
+        }
+
+
+        /// <summary>
+        /// 查询今天员工是否已签到
+        /// </summary>
+        /// <param name="wkn"></param>
+        /// <returns></returns>
+        public static object SelectToDayCheckInfoByWorkerNo(string wkn)
+        {
+            string sql = "select Count(*) from WORKERCHECK where WorkerNo = '"+wkn+"' and DateDiff(dd,CheckTime,getdate())=0";
+            return DBHelper.ExecuteScalar(sql);
+        }
+
+        /// <summary>
+        /// 添加员工打卡数据
+        /// </summary>
+        /// <param name="workerCheck"></param>
+        /// <returns></returns>
+        public static int AddCheckInfo(WorkerCheck workerCheck)
+        {
+            string sql = "insert into WORKERCHECK values('" + workerCheck.WorkerNo + "','"+workerCheck.CheckTime+"','" + workerCheck.CheckWay + "','0')";
+            return DBHelper.ExecuteNonQuery(sql);
+        }
+    }
+}
---
+++ SYS.Application/Worker/WorkerGoodBadService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace SYS.Application
+{
+    public class WorkerGoodBadService
+    {
+        public static int AddGoodBad(WorkerGoodBad goodBad)
+        {
+            string sql = "insert into WorkerGoodBad values('" + goodBad.WorkNo + "','" + goodBad.GBInfo + "','" + goodBad.GBType + "','" + goodBad.GBOperation + "','" + goodBad.GBTime + "')";
+            return DBHelper.ExecuteNonQuery(sql);
+        }
+
+
+        public static List<WorkerGoodBad> SelectAllGoodBadByWorkNo(string wn) 
+        {
+            List<WorkerGoodBad> gb = new List<WorkerGoodBad>();
+            string sql = "select * from WorkerGoodBad wgb,GBType gbt where wgb.GBType = gbt.GBType and wgb.WorkNo = '" + wn + "'";
+            SqlConnection conn = DBHelper.GetConnection();
+            conn.Open();
+            SqlDataReader dr = DBHelper.ExecuteReader(sql);
+            while (dr.Read())
+            {
+                WorkerGoodBad goodBad = new WorkerGoodBad();
+                goodBad.WorkNo = dr["WorkNo"].ToString();
+                goodBad.GBInfo = dr["GBInfo"].ToString();
+                goodBad.TypeName = dr["GBName"].ToString();
+                goodBad.GBOperation = dr["GBOperation"].ToString();
+                goodBad.GBTime = (DateTime)dr["GBTime"];
+                gb.Add(goodBad);
+            }
+            dr.Close();
+            DBHelper.Closecon();
+            return gb;
+        }
+    }
+}
