666208e59f7bd2a1dc77d897e590a6ea9bb66f0c
Chunkit Yeung <Jackson0720@outlook.com>
2021-01-06 23:00:12 +0800

    commit by jackson

---
+++ SYS.Common/DbContextOption.cs
@@ -0,0 +1,9 @@
+﻿namespace SYS.Common
+{
+    public class DbContextOption
+    {
+        public string ConnectionString { get; set; }
+
+        public string ModelAssemblyName { get; set; }
+    }
+}
