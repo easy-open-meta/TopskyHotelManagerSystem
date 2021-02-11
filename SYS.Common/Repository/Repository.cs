1c26d25373efd0eef1f131459b09ac59b6bfaa5b
Chunkit Yeung <Jackson0720@outlook.com>
2021-02-12 00:05:22 +0800

    InitProject_v1.4.8_happy_new_year

---
+++ SYS.Common/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYS.Common
+{
+    public class Repository<T> : SimpleClient<T> where T : class, new()
+    {
+        public Repository(ISqlSugarClient context = null) : base(context)//注意这里要有默认值等于null
+        {
+            if (context == null)
+            {
+                base.Context = new SqlSugarClient(new ConnectionConfig()
+                {
+                    DbType = SqlSugar.DbType.MySql,
+                    InitKeyType = InitKeyType.Attribute,
+                    IsAutoCloseConnection = true,
+                    ConnectionString = "Server=134.175.239.108;Database=tshoteldb;Uid=root;Pwd=yjj0720.;"
+                });
+
+                base.Context.Aop.OnLogExecuting = (s, p) =>
+                {
+                    Console.WriteLine(s);
+                };
+            }
+        }
+
+        /// <summary>
+        /// 扩展方法，自带方法不能满足的时候可以添加新方法
+        /// </summary>
+        /// <returns></returns>
+        public List<T> CommQuery(string json)
+        {
+            T t = Context.Utilities.DeserializeObject<T>(json);
+            var list = base.Context.Queryable<T>().WhereClass(t).ToList();
+            return list;
+        }
+    }
+}
+}
---
+++ SYS.Core/Room/RoomState.cs
@@ -0,0 +1,17 @@
+﻿namespace SYS.Core
+{
+    /// <summary>
+    /// 房间状态
+    /// </summary>
+    public static class RoomState
+    {
+        /// <summary>
+        /// 房间编号
+        /// </summary>
+        public static string RoomNo { get; set; }
+        /// <summary>
+        /// 房间状态编号
+        /// </summary>
+        public static int RoomStateId { get; set; }
+    }
+}
